a52ca6bd00e6d5c4e974ffc144bf4db04e363230
Amanullha Zoha <amanullhazoha3784@gmail.com>
2024-09-16 23:44:12 +0600

    modal issue solve

--- src/components/modals/AcceptTermsModal.jsx
+++ src/components/modals/AcceptTermsModal.jsx
@@ -1,6 +1,39 @@
-import logo from "../../assets/images/conqueror_logo.png"
+import logo from "../../assets/images/conqueror_logo.png";
+import { useState, useEffect, useRef } from "react";
 
 const AcceptTermsModal = ({ isOpen, onClose, handleAccept }) => {
+  const scrollElement = useRef(null);
+  const [language, setLanguage] = useState("en");
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+
+  const handleScroll = () => {
+    const element = scrollElement.current;
+    if (!element) return;
+
+    const scrollTop = element.scrollTop;
+    const scrollHeight = element.scrollHeight;
+    const clientHeight = element.clientHeight;
+
+    const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    
+    if(scrollPercentage >= 30) {
+        setIsButtonEnabled(true);
+    }
+
+  };
+
+  useEffect(() => {
+    if (scrollElement.current) {
+      scrollElement.current.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (scrollElement.current) {
+        scrollElement.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
@@ -10,12 +43,25 @@ const AcceptTermsModal = ({ isOpen, onClose, handleAccept }) => {
             <img src={logo} alt="logo" className="w-[150px] h-auto" />
 
             <div className="flex gap-0.5 items-center bg-slate-100 p-1 rounded-[30px]">
-                <button type="button" className="bg-[#1B345E] text-white px-3 py-2 rounded-[30px]">English</button>
-                <button type="button" className="px-3 py-2 rounded-[30px] text-[#111928]">عربي</button>
+                <button 
+                    type="button" 
+                    className={`${language === "en" ? "bg-[#1B345E] text-white" : "text-[#111928]"} px-3 py-2 rounded-[30px]`} 
+                    onClick={() => setLanguage("en")}
+                >
+                        English
+                </button>
+
+                <button 
+                    type="button" 
+                    className={`${language === "ab" ? "bg-[#1B345E] text-white" : "text-[#111928]"} px-3 py-2 rounded-[30px]`}
+                    onClick={() => setLanguage("ab")}
+                >
+                    عربي
+                </button>
             </div>
         </div>
 
-        <div className="h-[200px] overflow-y-auto">
+        <div className="h-[200px] overflow-y-auto" ref={scrollElement}>
             <p className="mb-4 text-[#111928] font-semibold text-sm md:text-base">Our Commitment to the Community</p>
             <h4 className="mb-6 text-[#111928] font-semibold text-lg md:text-2xl">At Conqueror Services, we strive to create a welcoming environment</h4>
             <p className="text-[#4B5563] font-normal text-base mb-6">To help us maintain this, we kindly ask you to commit to the following:</p>
@@ -36,7 +82,8 @@ const AcceptTermsModal = ({ isOpen, onClose, handleAccept }) => {
             <button 
                 type="button" 
                 onClick={() => handleAccept()}
-                className="w-full px-1.5 py-2.5 bg-[#9CA3AF] text-white rounded-full text-sm font-medium border border-[#E5E7EB]"
+                className={`w-full px-1.5 py-2.5 bg-[#9CA3AF] text-white rounded-full text-sm font-medium border border-[#E5E7EB] ${isButtonEnabled ? 'enabled' : 'disabled'}`}
+                disabled={!isButtonEnabled}
             >
                 Agree and continue
             </button>
